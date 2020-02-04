ca351ef0246c0b4ac205c5622290b85b26946ede
Will-Peterson <fishsticks91@yahoo.com>
2020-02-04 12:50:15 -0800

    designing footer layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// import Button from '@material-ui/core/Button
-
 function App() {
   return (
     <div className="App">
